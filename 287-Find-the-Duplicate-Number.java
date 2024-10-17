e03afab7c54c20935c937169fc4b47d2ab146f45
ARJUN-SUWEDI <arjunsuwedi24@gmail.com>
2024-10-17 18:01:02 +0530

    [287] [Time Beats: 100.00%] [Memory Beats: 28.84%] - LeetPush

--- 287-Find-the-Duplicate-Number.java
+++ 287-Find-the-Duplicate-Number.java
@@ -1,11 +1,20 @@
 class Solution {
     public int findDuplicate(int[] nums) {
-        Arrays.sort(nums);
+        /*Arrays.sort(nums);
         for(int i=0;i<nums.length-1;++i){
             if(nums[i]==nums[i+1]){
                 return nums[i];
             }
         }
-        return nums[0];
+        return nums[0];*/
+        boolean arr[]=new boolean[nums.length];
+        for(int n:nums){
+            if(arr[n]){
+                return n;
+            }
+            arr[n]=true;
+        }
+        return 404;
+
     }
 }

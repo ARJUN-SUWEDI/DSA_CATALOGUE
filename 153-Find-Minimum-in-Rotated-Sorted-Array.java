e2409e334f74b491b2a64c57f1d87a7889a5ecfe
ARJUN-SUWEDI <arjunsuwedi24@gmail.com>
2024-10-23 18:14:15 +0530

    [153] [Time Beats: 100.00%] [Memory Beats: 14.17%] - LeetPush

--- 153-Find-Minimum-in-Rotated-Sorted-Array.java
+++ 153-Find-Minimum-in-Rotated-Sorted-Array.java
@@ -1,6 +1,6 @@
 class Solution {
     public int findMin(int[] nums) {
-        for(int i=1;i<nums.length;++i){
+        /*for(int i=1;i<nums.length;++i){
             if(i==nums.length-1){
                 if(nums[i]<nums[i-1]&&nums[i]<nums[0]){
                     return nums[i];
@@ -11,6 +11,19 @@ class Solution {
                 return nums[i];
             }
         }
-        return nums[0];
+        return nums[0];*/
+
+        int l=0;
+        int r=nums.length-1;
+        while(l<r){
+            int mid=l+(r-l)/2;
+            if(nums[mid]>=nums[r]){
+                l=mid+1;
+            }
+            else{
+                r=mid;
+            }
+        }
+        return nums[l];
     }
 }

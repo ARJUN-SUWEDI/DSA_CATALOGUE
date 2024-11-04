d0bff0d375d56ee9019d729f4d8c11597e80d614
ARJUN-SUWEDI <arjunsuwedi24@gmail.com>
2024-11-05 03:00:20 +0530

    [258] [Time Beats: 100.00%] [Memory Beats: 45.29%] - LeetPush

--- 258-Add-Digits.java
+++ 258-Add-Digits.java
@@ -1,8 +1,22 @@
 class Solution {
     public int addDigits(int num) {
-        if (num == 0) {
-            return 0;
+        if(num<10){
+            return num;
         }
-        return (num % 9 == 0) ? 9 : (num % 9);
+        int count=0;
+        return add(num,count);
+    }
+    public static int add(int n, int c){
+        
+        if(n<10){
+            return n;
+        }
+        c++;
+        int s=0;
+        while(n>0){
+            s=s+(n%10);
+            n=n/10;
+        }
+        return add(s,c);
     }
 }

876e7a6cd7d18f3385f3cb4918a4781ae1d3a1b4
ARJUN SUWEDI <arjunsuwedi24@gmail.com>
2024-11-18 13:23:00 +0530

    [2942] [Time Beats: 100.00%] [Memory Beats: 64.67%] - LeetPush

--- 2942-Find-Words-Containing-Character.java
+++ 2942-Find-Words-Containing-Character.java
@@ -2,7 +2,7 @@ class Solution {
     public List<Integer> findWordsContaining(String[] words, char x) {
         List <Integer> array=new ArrayList<>();
         for(int i=0;i<words.length;++i){
-            if(words[i].contains(String.valueOf(x)))array.add(i);
+            if(words[i].indexOf(x)!=-1)array.add(i);
         }
         return array;
     }

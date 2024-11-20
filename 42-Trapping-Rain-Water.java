bd30e389a285123bb5409fb77abf1789898a213b
ARJUN SUWEDI <arjunsuwedi24@gmail.com>
2024-11-20 10:25:43 +0530

    [42] [Time Beats: 100.00%] [Memory Beats: 66.94%] - LeetPush

--- 42-Trapping-Rain-Water.java
+++ 42-Trapping-Rain-Water.java
@@ -1,19 +1,21 @@
 class Solution {
     public int trap(int[] height) {
-        int left=0;
-        int right=height.length-1;
-        int lm=height[left];
-        int rm=height[right];int water=0;
-        while(left<right){
+        int l=0;
+        int r=height.length-1;
+        int lm=height[l];
+        int rm=height[r];
+        int water=0;
+        while(l<r){
             if(lm<rm){
-                left++;
-                lm=lm>height[left]?lm:height[left];
-                water=water+lm-height[left];
+                l++;
+                lm=lm>height[l]?lm:height[l];
+                water=water+lm-height[l];
             }
             else{
-                right--;
-                rm=rm>height[right]?rm:height[right];
-                water=water+rm-height[right];
+                r--;
+                rm=rm>height[r]?rm:height[r];
+                water=water+rm-height[r];
+            
             }
         }
         return water;

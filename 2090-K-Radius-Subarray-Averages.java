e2b6c27c194439322bc52b0bc3ed6fc818b828da
ARJUN SUWEDI <arjunsuwedi24@gmail.com>
2024-11-21 22:46:25 +0530

    [2090] [Time Beats: 71.20%] [Memory Beats: 36.24%] - LeetPush

--- 2090-K-Radius-Subarray-Averages.java
+++ 2090-K-Radius-Subarray-Averages.java
@@ -3,9 +3,7 @@ class Solution {
         int n = nums.length;
         int[] ans = new int[n];
         Arrays.fill(ans, -1);
-        
         if (2*k+1 > n) return ans;
-        
         long ws = 0;
         for (int i = 0; i < 2*k+1; i++) ws += nums[i];
         ans[k] = (int)(ws/(2*k+1));

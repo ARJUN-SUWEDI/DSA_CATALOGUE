b11a9048289811523d72b79a91d123d48ddfccc0
ARJUN SUWEDI <arjunsuwedi24@gmail.com>
2024-12-28 15:30:43 +0530

    [1512] [Time Beats: 100.00%] [Memory Beats: 11.63%] - LeetPush

--- 1512-Number-of-Good-Pairs.cpp
+++ 1512-Number-of-Good-Pairs.cpp
@@ -1,5 +1,6 @@
 class Solution {
 public:
+    /* frugal n^2 approach
     int f(int x, vector<int>& nums){
         int c=0;
         for(int i=0;i<x;++i){
@@ -13,6 +14,16 @@ public:
         int ans=0;
         for(int x=0;x<nums.size();++x){
             ans+=f(x,nums);
+        }
+        return ans;
+    }*/
+    /*O(n) approach*/
+    int numIdenticalPairs(vector<int>& nums) {
+        int ans=0;
+        int count[101]={0};
+        for(int i=0;i<nums.size();++i){
+            ans+=count[nums[i]];
+            count[nums[i]]+=1;
         }
         return ans;
     }

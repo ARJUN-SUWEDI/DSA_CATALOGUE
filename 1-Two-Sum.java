242ad74bb935578f84718080991c490dfa95f0e6
ARJUN SUWEDI <arjunsuwedi24@gmail.com>
2025-01-09 13:55:48 +0530

    [1] [Time Beats: 98.92%] [Memory Beats: 14.50%] - LeetPush

--- 1-Two-Sum.java
+++ 1-Two-Sum.java
@@ -1,12 +1,12 @@
 class Solution {
     public int[] twoSum(int[] nums, int target) {
-        for(int i=1;i<nums.length;++i){
-            for(int j=i;j<nums.length;++j){
-                if(nums[j]+nums[j-i]==target){
-                   return new int[]{j,j-i};
-                }
+        HashMap<Integer,Integer> map=new HashMap<>();
+        for(int i=0;i<nums.length;++i){
+            if(map.containsKey(target-nums[i])){
+                return new int[]{map.get(target-nums[i]),i};
             }
+            map.put(nums[i],i);
         }
-        return null;
+        return new int[]{};
     }
 }

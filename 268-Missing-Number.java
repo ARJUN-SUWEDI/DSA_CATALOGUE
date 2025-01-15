f96df04d838a6d8baa01b27229b2af1d3a8d0b47
ARJUN SUWEDI <arjunsuwedi24@gmail.com>
2025-01-15 12:44:05 +0530

    [268] [Time Beats: 100.00%] [Memory Beats: 6.56%] - LeetPush

--- 268-Missing-Number.java
+++ 268-Missing-Number.java
@@ -1,11 +1,33 @@
 class Solution {
     public int missingNumber(int[] nums) {
-        int a=nums.length;
-        int n=((a*a)+a)/2;
-        int sum=0;
-        for(int i=0;i<nums.length;++i){
-            sum=sum+nums[i];
+        // int idx=0;
+        // while(idx<nums.length){
+        //     int correct=nums[idx];
+        //     if(nums[idx]<nums.length && nums[idx] != nums[correct]){
+        //         nums[idx]=nums[idx]^nums[correct];
+        //         nums[correct]=nums[idx]^nums[correct];
+        //         nums[idx]=nums[idx]^nums[correct];
+        //     }else{
+        //         idx++;
+        //     }
+        // }
+        // for(int i=0;i<nums.length;++i){
+        //     if(nums[i]!=i){
+        //         return i;
+        //     }
+        // }
+        // return nums.length;
+
+        int xorA=0;
+        int xorB=0;
+        int n=nums.length;
+        for(int i=0;i<=n;++i){
+            xorA^=i;
+            if(i<n){
+                xorB^=nums[i];
+            }
         }
-        return n-sum;
+        
+        return xorA^xorB;
     }
 }

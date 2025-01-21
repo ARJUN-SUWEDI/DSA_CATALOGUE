ab95752d465368b4e53a93e867b0984e1d4986bf
ARJUN SUWEDI <arjunsuwedi24@gmail.com>
2025-01-21 16:19:02 +0530

    [20] [Time Beats: 42.21%] [Memory Beats: 79.89%] - LeetPush

--- 20-Valid-Parentheses.java
+++ 20-Valid-Parentheses.java
@@ -1,16 +1,16 @@
 class Solution {
     public boolean isValid(String s) {
         Stack<Character> stack = new Stack<>();
+        Map<Character, Character> map = new HashMap<>();
+        map.put(')', '(');
+        map.put('}', '{');
+        map.put(']', '[');
 
         for (char c : s.toCharArray()) {
-            if(c=='('||c=='['||c=='{'){
+            if (map.containsValue(c)) {
                 stack.push(c);
-            }
-            else{
-                
-                if(stack.isEmpty())return false;
-                char a=stack.pop();
-                if((c==')' && a!='(')||(c==']'&&a!='[')||(c=='}'&&a!='{')){
+            } else if (map.containsKey(c)) {
+                if (stack.isEmpty() || map.get(c) != stack.pop()) {
                     return false;
                 }
             }

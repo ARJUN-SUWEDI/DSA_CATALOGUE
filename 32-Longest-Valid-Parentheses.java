2463d96f4532195f6db5e745d854a707a8c38c41
ARJUN SUWEDI <arjunsuwedi24@gmail.com>
2025-01-21 16:38:55 +0530

    [32] [Time Beats: 75.73%] [Memory Beats: 15.38%] - LeetPush

--- 32-Longest-Valid-Parentheses.java
+++ 32-Longest-Valid-Parentheses.java
@@ -1,21 +1,19 @@
 class Solution {
     public int longestValidParentheses(String s) {
-        Stack<Integer> stack = new Stack<>();
-        stack.push(-1);
-        int max = 0;
-
-        for (int i = 0; i < s.length(); i++) {
-            char a=s.charAt(i);
-            if(a=='(')stack.push(i);
+        Stack<Integer> st=new Stack<>();
+        int max=0;
+        st.push(-1);
+        for(int i=0;i<s.length();++i){
+            char c=s.charAt(i);
+            if(c=='('){
+                st.push(i);
+            }
             else{
-                stack.pop();
-                if(stack.isEmpty())stack.push(i);
-                else{
-                    max=Math.max(max,i-stack.peek());
-                }
+                st.pop();
+                if(st.isEmpty())st.push(i);
+                max=Math.max(max,i-st.peek());
             }
         }
-
         return max;
     }
 }

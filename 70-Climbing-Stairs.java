9593d9fa01d7e54a545417f59ce9f9cc938f6a22
ARJUN SUWEDI <arjunsuwedi24@gmail.com>
2025-01-30 11:06:53 +0530

    [70] [Time Beats: 100.00%] [Memory Beats: 65.22%] - LeetPush

--- 70-Climbing-Stairs.java
+++ 70-Climbing-Stairs.java
@@ -1,14 +1,16 @@
 class Solution {
     public int climbStairs(int n) {
-        int[] d_p=new int[n+1];
-        if(n==1){
+        if(n<=1){
             return 1;
         }
-        d_p[1]=1;
-        d_p[2]=2;
-        for(int i=3;i<=n;++i){
-            d_p[i]=d_p[i-1] + d_p[i-2];
+        int a=1;
+        int b=1;
+        int c=0;
+        for(int i=2;i<=n;++i){
+            c=a+b;
+            a=b;
+            b=c;
         }
-        return d_p[n];
+        return c;
     }
 }

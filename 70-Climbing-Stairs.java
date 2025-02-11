b05db68ad07ae9ba7618707fcdab50f99d63c6d3
ARJUN SUWEDI <arjunsuwedi24@gmail.com>
2025-02-11 19:40:10 +0530

    [70] [Time Beats: 100.00%] [Memory Beats: 50.11%] - LeetPush

--- 70-Climbing-Stairs.java
+++ 70-Climbing-Stairs.java
@@ -5,12 +5,11 @@ class Solution {
         }
         int a=1;
         int b=1;
-        int c=0;
         for(int i=2;i<=n;++i){
-            c=a+b;
+            int c=a+b;
             a=b;
             b=c;
         }
-        return c;
+        return b;
     }
 }

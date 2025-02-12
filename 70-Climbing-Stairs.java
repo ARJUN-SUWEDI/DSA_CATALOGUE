d9fe4017221c14339d07aa45743e0c58ea33488a
ARJUN SUWEDI <arjunsuwedi24@gmail.com>
2025-02-12 11:33:19 +0530

    [70] [Time Beats: 100.00%] [Memory Beats: 98.10%] - LeetPush

--- 70-Climbing-Stairs.java
+++ 70-Climbing-Stairs.java
@@ -1,17 +1,27 @@
 class Solution {
     public int climbStairs(int n) {
-        int [] dp=new int[n+1];
-        Arrays.fill(dp,-1);
-        return memorization(n,dp);
-    }
-    public int memorization(int n, int [] dp){
-        if(n<=2){
-            return n;
+        // int [] dp=new int[n+1];
+        // Arrays.fill(dp,-1);
+        // return memorization(n,dp);
+        if(n<=2)return n;
+        int prev1=2;
+        int prev2=1;
+        for(int i=3;i<=n;++i){
+            int a=prev1+prev2;
+            prev2=prev1;
+            prev1=a;
         }
-        if(dp[n]!=-1){
-            return dp[n];
-        }
-        dp[n]=memorization(n-1,dp)+memorization(n-2,dp);
-        return dp[n];
+        return prev1;
+
     }
+    // public int memorization(int n, int [] dp){
+    //     if(n<=2){
+    //         return n;
+    //     }
+    //     if(dp[n]!=-1){
+    //         return dp[n];
+    //     }
+    //     dp[n]=memorization(n-1,dp)+memorization(n-2,dp);
+    //     return dp[n];
+    // }
 }

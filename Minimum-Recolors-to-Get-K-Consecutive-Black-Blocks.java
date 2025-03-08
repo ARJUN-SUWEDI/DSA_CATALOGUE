5c9994d7e204ac703bcc6d8cdbbd03444b12755a
ARJUN SUWEDI <arjunsuwedi24@gmail.com>
2025-03-08 20:24:06 +0530

    [2379] [Time Beats: 17.03%] [Memory Beats: 18.39%] - LeetPush

---
+++ Minimum-Recolors-to-Get-K-Consecutive-Black-Blocks.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int minimumRecolors(String blocks, int k) {
+        int n = blocks.length();
+        int minOperations = Integer.MAX_VALUE;
+        
+        for (int i = 0; i <= n - k; i++) {
+            int whiteCount = 0;
+            
+            for (int j = i; j < i + k; j++) {
+                if (blocks.charAt(j) == 'W') {
+                    whiteCount++;
+                }
+            }
+            
+            // Update the minimum number of operations
+            minOperations = Math.min(minOperations, whiteCount);
+        }
+        return minOperations;   
+    }
+}

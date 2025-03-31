1e89abd9ab2c3ddea114b928e9293182f66c7e0b
ARJUN SUWEDI <arjunsuwedi24@gmail.com>
2025-03-31 11:40:45 +0530

    [2551] [Time Beats: 84.33%] [Memory Beats: 20.15%] - LeetPush

---
+++ Put-Marbles-in-Bags.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int[] applyOperations(int[] nums) {
+        int[] arr=new int[nums.length];
+        for(int i=0;i<nums.length-1;i++){
+            if(nums[i]==nums[i+1]){
+                nums[i]=nums[i]*2;
+                nums[i+1]=0;
+                i++;
+            }
+
+        }
+        int k=0;
+        for(int i=0;i<nums.length;i++){
+            if(nums[i]!=0){
+                arr[k++]=nums[i];
+            }
+        }
+        
+  return arr;
+    }
+}
